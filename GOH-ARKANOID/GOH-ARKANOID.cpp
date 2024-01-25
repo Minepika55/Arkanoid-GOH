b03399ef44fe69a5ced20084cc131aaf0c9e3df7
Minepika55 <98884543+Minepika55@users.noreply.github.com>
2024-01-25 18:18:35 +0100

    Update GOH-ARKANOID.cpp

--- GOH-ARKANOID/GOH-ARKANOID.cpp
+++ GOH-ARKANOID/GOH-ARKANOID.cpp
@@ -12,33 +12,33 @@ int main() {
     Vector2f viewSize(x_max, y_max);
     VideoMode vm(viewSize.x, viewSize.y);
     RenderWindow window(vm, title, Style::Default);
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(60); //Aquesta part d'aqui serveix per regular les frames del joc les uqlas intentaran sempre ser 60 per segon.
 
-    //Definim com es la pilota i com es mou la pilota reacciona amb el contacte amb les pareys de la finestra i els blocs.
+    //Definim com es la pilota i com reacciona amb el contacte amb les parets de la finestra i els blocs.
     Texture bolaTex;
-    bolaTex.loadFromFile("graphics/bola.png");
+    bolaTex.loadFromFile("graphics/bola.png"); //Aqui es carrega la textura de la pilota.
     Sprite spritepilota;
     spritepilota.setTexture(bolaTex);
     FloatRect pilotaRect = spritepilota.getLocalBounds();
-    spritepilota.setOrigin(pilotaRect.left + pilotaRect.width / 2.0f, pilotaRect.top + pilotaRect.height / 2.0f);
-    spritepilota.setPosition(viewSize.x / 2, viewSize.y / 2);
+    spritepilota.setOrigin(pilotaRect.left + pilotaRect.width / 2.0f, pilotaRect.top + pilotaRect.height / 2.0f); 
+    spritepilota.setPosition(viewSize.x / 2, viewSize.y / 2); //Aqui s'estableix el tamany de la pilota.
 
-    // Definim la barra del jugador la qual reacciona al input del jugador i es pot moure d'esquerra a dreta.
+    //Definim la barra del jugador la qual reacciona al input del jugador i es pot moure d'esquerra a dreta.
     Texture playerTex;
-    playerTex.loadFromFile("graphics/raqueta.png");
+    playerTex.loadFromFile("graphics/raqueta.png"); //Aqui es carrega la textura de la barra.
     Sprite spriteplayer;
     spriteplayer.setTexture(playerTex);
     FloatRect playerRect = spriteplayer.getLocalBounds();
     spriteplayer.setOrigin(playerRect.left + playerRect.width / 2.0f, playerRect.top + playerRect.height / 2.0f);
-    spriteplayer.setPosition(viewSize.x / 2, viewSize.y - 50);
+    spriteplayer.setPosition(viewSize.x / 2, viewSize.y - 50); //Aqui s'estableix el tamany de la barra.
 
-    // Definim la velocitat inicial de la pilota a la qual es moura constantment.
+    //Definim la velocitat inicial de la pilota a la qual es moura constantment i sense parar.
     float bolaSpeedX, bolaSpeedY;
     bolaSpeedY = bolaSpeedBase * 1;
     bolaSpeedX = bolaSpeedBase * 1.7;
     sf::Clock clock;
 
-    //Aqui carrego la lletra a utilitzar per el HUD i el Game Over.
+    //Aqui carrego la tipografia per el HUD i el Game Over.
     sf::Font font;
     if (!font.loadFromFile("fonts/PLANK.TTF")) {
 
@@ -47,24 +47,26 @@ int main() {
     //Aquesta part d'aqui crea els blocs a la part de dalt un al costat de l'altre.
     std::vector<Sprite> blocs(numBlocs);
     Texture blocTex;
-    blocTex.loadFromFile("graphics/bloc.png");
+    blocTex.loadFromFile("graphics/bloc.png"); //Aquesta part carrega la textura utilitzada per tots els blocs.
     for (int i = 0; i < numBlocs; ++i) {
         blocs[i].setTexture(blocTex);
-        blocs[i].setPosition(i * 100, 50);
+        blocs[i].setPosition(i * 100, 50); //Aquesta part controla la separacio entre els blocs i la seva posicio a la pantalla.
     }
-
+    
+    //Aquesta part defineix com es mostra el text de game over.
     sf::Text GameOverMarcador;
     GameOverMarcador.setFont(font);
     GameOverMarcador.setString("Game Over");
-    GameOverMarcador.setCharacterSize(50);
-    GameOverMarcador.setFillColor(sf::Color::Red);
-
+    GameOverMarcador.setCharacterSize(50); //Aqui es controla el tamany del text.
+    GameOverMarcador.setFillColor(sf::Color::Red); //Aqui es controla el color del text.
+    
+    //Aquesta part defineix com es mostra el text de la puntuacio.
     sf::Text PuntsMarcador;
     PuntsMarcador.setFont(font);
     PuntsMarcador.setString("Punts: " + std::to_string(punts));
-    PuntsMarcador.setCharacterSize(30);
-    PuntsMarcador.setFillColor(sf::Color::Blue);
-    PuntsMarcador.setPosition(viewSize.x / 80, viewSize.y / -200);
+    PuntsMarcador.setCharacterSize(30); //Aqui es controla el tamany del text.
+    PuntsMarcador.setFillColor(sf::Color::Blue); //Aqui es controla el color del text.
+    PuntsMarcador.setPosition(viewSize.x / 80, viewSize.y / -200); //Aqui es controla la posicio del text.
 
 
     //Tot aquest codi s'executa quan la finestra esta oberta.
